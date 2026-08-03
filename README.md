965c90326a6c717ad361f76994822b93c6dc0004
yifanchen <yifanchen@synology.com>
2026-08-03 19:14:42 +0800

    docs: update after project rename

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# Tampermonkey: Steamgifts sheet fetcher
+# Tampermonkey: Steamgifts Helper
 
 ## Installation
 
 1. Install [Violentmonkey](https://addons.mozilla.org/en-us/firefox/addon/violentmonkey/) (Firefox) or [Tampermonkey](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo) (Chrome) / [Tampermonkey](https://addons.opera.com/en/extensions/details/tampermonkey-beta/) (Opera).
-2. Open `https://github.com/YiFanChen99/tampermonkey--steamgifts-sheet-fetcher/raw/main/Script.user.js` or click [here](https://github.com/YiFanChen99/tampermonkey--steamgifts-sheet-fetcher/raw/main/Script.user.js)
+2. Open `https://github.com/YiFanChen99/tampermonkey--steamgifts-helper/raw/main/Script.user.js` or click [here](https://github.com/YiFanChen99/tampermonkey--steamgifts-helper/raw/main/Script.user.js)
 3. Click `Install`.
--- Script.meta.js
+++ Script.meta.js
@@ -7,7 +7,7 @@
 // @match        *://www.steamgifts.com/giveaways/search*
 // @match        *://www.steamgifts.com/giveaway/*
 // @grant        GM_xmlhttpRequest
-// @icon         https://raw.githubusercontent.com/YiFanChen99/tampermonkey--steamgifts-sheet-fetcher/main/favicon.ico
-// @downloadURL  https://raw.githubusercontent.com/YiFanChen99/tampermonkey--steamgifts-sheet-fetcher/main/Script.user.js
-// @updateURL    https://raw.githubusercontent.com/YiFanChen99/tampermonkey--steamgifts-sheet-fetcher/main/Script.meta.js
+// @icon         https://raw.githubusercontent.com/YiFanChen99/tampermonkey--steamgifts-helper/main/favicon.ico
+// @downloadURL  https://raw.githubusercontent.com/YiFanChen99/tampermonkey--steamgifts-helper/main/Script.user.js
+// @updateURL    https://raw.githubusercontent.com/YiFanChen99/tampermonkey--steamgifts-helper/main/Script.meta.js
 // ==/UserScript==
